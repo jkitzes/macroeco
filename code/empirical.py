f4c58bb2f1b59ac447d7c213fe697a719627053e
Justin Kitzes <jkitzes@berkeley.edu>
2012-02-08 13:09:32 -0800

    Add module docstring and comments, rename class Plot to Patch

--- code/empirical.py
+++ code/empirical.py
@@ -1,114 +1,69 @@
 '''
-Python program to examine SAR and EAR
+Routines for calculating empirical macroecological metrics.
+
+This module primarily declares a class Patch that represents a single, 
+spatially located ecological survey (such as the STRI BCI forest plot, 
+serpentine grassland plot, etc). Patch has many methods that are used to 
+calculate macroecological metrics.
+
+Classes
+-------
+- `Patch` - a single spatially and temporally identified data set
+
+Patch Methods
+-------------
+- `SAD_grid` - calculate gridded SAD, SAR, or EAR
+- `SAD_sample` - calculate sampled SAD, SAR, or EAR
+- `QS_grid` - calculate gridded commonality for pairs of subpatches
+- `sub_SAD` - calculate SAD, spp count, or endemics count for subset of patch
+- `get_nspp` - count number of species in sparse or dense data
+- `get_sparse_bool` - return True if patch data is in sparse format
+- `get_total_abund` - count total abundance of each species in patch
+
+Misc functions
+--------------
+- `sample` - return random element from 1D numpy array
+- `distance` - return Euclidean distance between two points
 '''
 
 import numpy as np
 import numpy.random as rand
-
-# UTILITY METHODS
-def sample(array):
-    '''
-    Returns random element from 1D numpy array.
-    Equivalent to np.random.choice in Numpy v1.7
-    '''
-    return array[rand.randint(0,len(array))]
-
-def distance(pt1, pt2):
-    ''' Calculate Euclidean distance between two points '''
-    return np.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
-
-
-# CONVERSION UTILITIES
-def xytable_to_sparse(xytable):
-    ''' Convert xytable data into sparse plot data '''
-    sparse_row = [np.array((None, None, None))]  # Dummy row, removed later,
-    sparse_count = [None]                        # so '==' always works
-    
-    for row in xytable:
-        if ~np.all(sparse_row == row, axis = 1).any():
-            sparse_row.append(row); sparse_count.append(1)
-        else:
-            row_ind = np.where(np.all(sparse_row == row, axis = 1))[0][0]
-            sparse_count[row_ind] += 1
-
-    return np.vstack((np.array(sparse_row[1:]).transpose(), 
-                      np.array(sparse_count[1:]))).transpose()
-
-
-def dense_to_sparse(dense, unit):
-    ''' Convert dense plot data into sparse plot data '''
-    sp_y = dense.shape[0]
-    sp_x = dense.shape[1]
-    nspp = dense.shape[2]
-
-    sparse = []
-    for spp in xrange(0, nspp):
-        for x in xrange(0, sp_x):
-            for y in xrange(0, sp_y):
-                if dense[y, x, spp] > 0:
-                    sparse.append([spp, x * unit, y * unit, dense[y, x, spp]])
-
-    return np.array(sparse)
-
-
-def sparse_to_dense(sparse, x_minmax, y_minmax, unit, nspp):
-    ''' Convert dense plot data into sparse plot data '''
-    x_min = x_minmax[0]
-    y_min = y_minmax[0]
-    nx = (x_minmax[1] - x_minmax[0] + unit) / float(unit)
-    ny = (y_minmax[1] - y_minmax[0] + unit) / float(unit)
-    
-    dense = np.zeros((ny, nx, nspp))
-    for row in sparse:
-        dense[(row[2]-y_min)/unit, (row[1]-x_min)/unit, row[0]] += row[3]
-    
-    return dense
+import data
+reload(data)
 
 
 # Temporarily define sample sparse and dense plots for testing
 test_dense = np.array((0,0,0,1,1,3,0,0,4,1,0,0,0,2,2,1)*2).reshape(4,4,2)
 test_dense[1,0,0] = 0
-test_sparse = dense_to_sparse(test_dense, 0.1)
+test_sparse = data.dense_to_sparse(test_dense, 0.1)
 test_xy = test_sparse[:,0:3]
 test_xy = np.vstack((test_xy,test_xy[0:8,:]))
 
 
-# LOAD_PLOT - Load plot data from file
-def load_plot(filename):
+#
+# CLASS PATCH
+#
+class PATCH:
     '''
-    Load plot and metadata from file. Data should be returned as a 3D numpy 
-    array if dense, or a 4 col numpy array if sparse. If sparse, first col must 
-    be an int index of species number, and function should also load the 
-    recarray that shows the match between index and spp name/code as given in 
-    the original data.
+    Patch class to store abundance data for multiple species in a single 
+    spatially and temporally identified location. Multiple non-contiguous 
+    patches or temporal series of data from a single patch should be declared 
+    as class Network.
     '''
-    # TODO: Add loading and parsing metadata
-    # TODO: Currently only takes in comma delimited, allow other options
-    # TODO: Currently only takes in sparse - may be harder to read grid data in 
-    # file - what format will it be?
-    try:
-        data = np.loadtxt(filename, skiprows = 1, delimiter = ',')
-    except IOError as detail:
-        print detail
-    
-    return data
+    # TODO: Configure to work with 3D data sets
 
-
-# CLASS PLOT - Define plot class
-class Plot:
-    '''
-    Plot class to store abundance data for multiple species in contiguous 
-    subplots. Multiple non-contiguous plots, each of which may contain 
-    subplots, should be declared as class Network.
-    '''
-
-    def __init__(self, data, x_minmax, y_minmax, nspp, sparse = True,
-                 unit = 0.1):
+    def __init__(self, data, x_minmax, y_minmax, unit = 0.1):
         '''
-        x_minmax and y_minmax are tuples giving the lowest and highest possible 
-        subplot centroid, respectively, nspp is number of species 
+        Initialize object of class Patch.
         '''
+        # TODO: Error checking for correct plot type
+        # TODO: Take extracted metadata fields and loaded data as args
+
         self.data = data
+        self.unit = unit
+        self.sparse = self.get_sparse_bool()
+        self.nspp = self.get_nspp()
+        self.total_abund = self.get_total_abund()
 
         self.x_min = x_minmax[0]
         self.x_max = x_minmax[1]
@@ -118,34 +73,22 @@ class Plot:
         self.y_max = y_minmax[1]
         self.p_height = self.y_max - self.y_min + unit  # Height in num units
 
-        self.nspp = nspp
-        self.sparse = sparse
-        self.unit = unit
-
-        if self.sparse:
-            self.total_abund = self.sparse_abund(data)
-        else:
-            self.total_abund = self.data.sum(axis=0).sum(axis=0)
-
-        # TODO: Error checking for correct plot type
-        # TODO: Change name Plot to something more unique - grid, tract, area?
-        # TODO: Convert arguments after data to metadata input 
-
 
     def SAD_grid(self, div_list, summary = ''):
         '''
-        Calculate gridded SADs from subplots created by evenly dividing a plot 
+        Calculate gridded SADs from subplots created by evenly dividing a patch 
         along the vertical and horizontal dimensions, with numbers of divisions 
         given by tuples div in div_list, wher div = (x_divs, y_divs). A 
-        division of one corresponds to no cut (ie, the whole plot).
+        division of one corresponds to no cut (ie, the whole patch).
 
         summary takes three arguments: '' (full SAD), 'SAR', and 'EAR'
 
-        Because of rounding, x_ and y_ divs that do not evenly split the plot 
-        in integer number of units will lead to subplots with different numbers 
-        of sample points, artificially inflating the variance of the species 
-        abundance or counts of the subplots. To avoid this, the values of x_ 
-        and y_divs must divide each dimension into an even number of units.
+        Because of rounding, x_ and y_ divs that do not evenly split the patch 
+        in integer number of units will lead to subpatches with different 
+        numbers of sample points, artificially inflating the variance of the 
+        species abundance or counts of the subpatches. To avoid this, the 
+        values of x_ and y_divs must divide each dimension into an even number 
+        of units.
         '''
         # TODO: Error check that div must be >= 1
 
@@ -166,8 +109,8 @@ class Plot:
                     y_st = self.y_min + y_div_count * sp_height
                     y_en = y_st + sp_height
 
-                    div_result.append(self.subSAD(x_st, x_en, y_st, y_en, 
-                                                  summary))
+                    div_result.append(self.sub_SAD(x_st, x_en, y_st, y_en, 
+                                                   summary))
 
             result.append(np.array(div_result))
 
@@ -198,8 +141,7 @@ class Plot:
                 x_en = x_st + sp_width
                 y_en = y_st + sp_height
 
-                wh_result.append(self.subSAD(x_st, x_en, y_st, y_en, 
-                                             summary))
+                wh_result.append(self.sub_SAD(x_st, x_en, y_st, y_en, summary))
 
             result.append(np.array(wh_result))
 
@@ -208,15 +150,17 @@ class Plot:
 
     def QS_grid(self, div_list):
         '''
-        Calculates commonality between pairs of subplots in a gridded plot as 
-        the Sorensen index (equivalent to Chi in Harte et al. papers, and 1 - 
-        Bray-Curtis dissimilarity).
+        Calculates commonality between pairs of subpatches in a gridded patch 
+        as the Sorensen index (equivalent to Chi in Harte et al. papers, and 1 
+        - Bray-Curtis dissimilarity).
 
         In result, the first two cols are the plot indices, indexed in single 
         sequence rowwise.
         '''
         # TODO: Make sure that divs divide plot evenly and that divs are of
         # width at least one unit.
+        # TODO: Failed with test_dense?
+        # TODO: Arg to spit out individ species, not Sorensen
 
         result = []
 
@@ -245,9 +189,38 @@ class Plot:
 
         return result
 
+
+    def sub_SAD(self, x_st, x_en, y_st, y_en, summary):
+        '''
+        Calculates a SAD for a subplot of known starting and ending 
+        coordinates. Only works for rectangles. If full, returns a ndarray of 
+        all species with abundances of each, else returns an int count of the 
+        species in the subplot.
+        '''
+        
+        if self.sparse:
+            # TODO: Use masked array instead of this bool?
+            in_sp = np.all([self.data[:,1] >= x_st, self.data[:,1] < x_en,
+                           self.data[:,2] >= y_st, self.data[:,2] < y_en],
+                           axis = 0)
+            sp_data = self.data[in_sp]
+            sub_abund = self.get_sparse_abund(sp_data)
+                
+        else:
+            sub_abund = self.data[y_st:y_en, x_st:x_en, 
+                                  :].sum(axis=0).sum(axis=0)
+
+        if summary is 'SAR':
+            return sum(sub_abund > 0)
+        elif summary is 'EAR':
+            return sum(sub_abund == self.total_abund)
+        else:
+            return sub_abund
+
+
     def get_sp_centers(self, div_list):
         '''
-        Get coordinate of center of plots in landscape gridded according to 
+        Get coordinate of center of patches in landscape gridded according to 
         divisions in div_list
         '''
         # TODO: Did not confirm that this works for x_min and y_min > 0
@@ -271,42 +244,47 @@ class Plot:
         return sp_centers
 
 
-    def subSAD(self, x_st, x_en, y_st, y_en, summary):
-        '''
-        Calculates a SAD for a subplot of known starting and ending 
-        coordinates. Only works for rectangles. If full, returns a ndarray of 
-        all species with abundances of each, else returns an int count of the 
-        species in the subplot.
-        '''
-        
+    def get_total_abund(self):
+        ''' Calculate total abundance of each species in entire patch '''
         if self.sparse:
-            in_sp = np.all([self.data[:,1] >= x_st, self.data[:,1] < x_en,
-                           self.data[:,2] >= y_st, self.data[:,2] < y_en],
-                           axis = 0)
-            sp_data = self.data[in_sp]
-            sub_abund = self.sparse_abund(sp_data)
-                
+            return self.sparse_abund(self.data)
         else:
-            sub_abund = self.data[y_st:y_en, x_st:x_en, 
-                                  :].sum(axis=0).sum(axis=0)
+            self.total_abund = self.data.sum(axis=0).sum(axis=0)
 
-        if summary is 'SAR':
-            return sum(sub_abund > 0)
-        elif summary is 'EAR':
-            return sum(sub_abund == self.total_abund)
+
+    def get_nspp(self):
+        ''' Get total number of species in self.data '''
+        if self.sparse:
+            return max(self.data[:,0])
         else:
-            return sub_abund
+            return np.size(self.data, 2)  # Size of 3rd dim
 
 
-    def sparse_abund(self, data):
-        ''' Calculate abundance of each species in sparse plot data '''
+    def get_sparse_bool(self):
+        ''' Return true if data is sparse, false if dense '''
+        return len(self.data.shape) == 2  # If data is 2D, is sparse
+
+
+    def get_sparse_abund(self, data):
+        ''' Calculate abundance of each species in a sparse patch '''
         abund = np.zeros(self.nspp)
         for row in data:
             abund[row[0]] += row[3]
         return abund
 
 
-# CLASS NETWORK - Network of plots - NOT YET IMPLEMENTED
-class Network():
-    pass
+#
+# MISC FUNCTIONS
+#
 
+def sample(array):
+    ''' Returns random element from 1D numpy array.
+
+    Equivalent to np.random.choice in Numpy v1.7
+    '''
+    return array[rand.randint(0, len(array))]
+
+
+def distance(pt1, pt2):
+    ''' Calculate Euclidean distance between two points '''
+    return np.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
