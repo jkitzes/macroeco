e02684376715a9886cbe664a8dae711f6cdbf15b
Justin Kitzes <jkitzes@berkeley.edu>
2012-05-28 11:25:46 -0700

    "Update data to accomodate variable column names."

--- macroeco/empirical/data.py
+++ macroeco/empirical/data.py
@@ -13,174 +13,105 @@ Functions
 '''
 
 from __future__ import division
-import numpy as np
 import warnings
-from metadata import *
-import os
+import numpy as np
+from matplotlib.mlab import csv2rec
+from macroeco.utility import metadata
 
 
-class XYTable():
+class DataTable():
     '''
-    Class to hold xytable data and parameters.
+    Class to hold data table and metadata.
 
     Parameters
     ----------
     datapath : str
         Path to data. Location of metadata determined from this path.
-    compress : bool
-        Flag to compress data so that there is only one row in xytable for each 
-        combination of spp and point coordinate.
+    params : dict
+        Params dictionary.
 
     Attributes
     ----------
-    table : ndarray
-        xytable data with cols for spp_code, x, y, etc. No header.
-    head : list
-        Headers for cols of table.
+    table : recarray
+        Census data loaded from csv
     meta : dict
         Dictionary of metadata associated with table.
-    spp_codes : ndarray
-        1D array of all specie codes in patch.
-    max_spp_code : int
-        Maximum value of species code found in xy.
     '''
 
-    def __init__(self, datapath, compress = False):
+    def __init__(self, datapath, params):
+        '''Initialize XYTable object. See class docstring.'''
+
+        self.table, self.meta = self.dataload(datapath, params)
+        self.spp_list = np.unique(self.table[params['spp_col_name']])
+
+    def dataload(self, datapath, params):
         '''
-        Initialize XYTable object. See class docstring.
-        '''
-
-        self.table, self.head, self.meta = self.xytable_load(datapath, 
-                                                             compress)
-
-        self.col_spp_code = self.head.index('spp_code')
-        self.col_count = self.head.index('count')
-
-        self.spp_codes = np.unique(self.table[:, self.col_spp_code])
-        self.max_spp_code = max(self.spp_codes)
-
-    def get_col_biomass(self):
-        '''
-        Trys to get biomass column. Throws error if it does not exist
-        '''
-        try:
-            self.col_biomass = self.head.index('biomass')
-        except ValueError:
-            raise ValueError("Column 'biomass' does not exist")
-
-
-    def xytable_load(self, datapath, compress):
-        '''
-        Load xytable table and metadata from file.
+        Load table and metadata from files.
         
         Parameters
         ----------
         datapath : str
-            Path to xytable table file. File must have header row and include 
-            cols, at a minimum, named int_code, x, and y.
-        compress : bool
-            Flag to compress data so that there is only one row in xytable for 
-            each combination of spp and point coordinate.
+            Path to data table file.
+        params : dict
+            Params dictionary.
             
         Returns
         -------
-        xy_table : 2D ndarray
-            Array of xytable data with cols specified in xy_head.
-        xy_head : list
-            List of headers for cols in xy_data.
+        table : recarray
+            Census data loaded from csv.
         meta : dict
-            Dictionary of metadata needed for analysis of xy_data. If no 
-            metadata file found, returns None.
+            Dictionary of metadata associated with table.
         '''
         
-        # Check that file is .csv. Not strictly necessary (only required that
-        # delimiter be a comma.
-        if datapath[-3:] != 'csv':
-            raise NotImplementedError('File must be .csv')
+        # Check that file is .csv.
+        assert datapath[-3:] == 'csv', 'File must be .csv'
 
-        # Attempt to load main table
-        xy_table = np.loadtxt(datapath, delimiter = ',', skiprows = 1)
+        # Load main table
+        table = csv2rec(datapath)
 
-        # Attempt to load header of data file
-        with open(datapath) as file:
-            xy_head = file.next()
-        xy_head = xy_head.replace(' ','').replace('\n','').replace('\r','')
-        xy_head = xy_head.split(',')
+        # Load metadata from file, printing warning and returning dict of zeros 
+        # if file does not exist.
+        metadatapath = datapath[:-3] + 'xml'
 
-        # Check that there are cols in data named spp_code, x, y
-        req_cols = set(['spp_code', 'x', 'y'])
-        cols = set(xy_head)
-        if cols.intersection(req_cols) != req_cols:
-            raise NameError('Data missing required column.')
+        asklist = []
+        for name in table.dtype.names:
+            asklist.append((name, 'minimum'))
+            asklist.append((name, 'maximum'))
+            asklist.append((name, 'precision'))
 
-        # Add count col if not already present
-        xy_table, xy_head = self.xytable_add_count(xy_table, xy_head, 
-                                                   compress)
-
-        # Load metadata from file, printing warning and returning None if file 
-        # does not exist.
         try:
-            metadata = datapath[:-3] + 'xml'
-            if os.path.isfile(metadata):
-                asklist = [('x', 'minimum'), ('x', 'maximum'), ('x', 'precision'),\
-                           ('y', 'minimum'), ('y', 'maximum'), ('y', 'precision')]
-                meta = self.fill_metadata(asklist, datapath)
-            else:
-                raise IOError()
+            meta_dict = self.get_metadata(asklist, metadatapath)
         except IOError:
-            warnings.warn('No metadata file found, metadata filled with 0s.')
-            meta = {'precision': 0, 'xrange': (0, 0), \
-                    'yrange': (0, 0)}
-            
-        # Check meta has required keys
-        req_keys = set(['precision', 'xrange', 'yrange'])
-        keys = set(meta.keys())
-        if keys.intersection(req_keys) != req_keys:
-            raise NameError('Metadata missing required parameter.')
+            meta_dict = {}
+            for id in asklist:
+                meta_dict[id] = None
+            warnings.warn('No metadata file found.')
 
-        return xy_table, xy_head, meta
-        
+        return table, meta_dict
 
-    def xytable_add_count(self, xy_in_data, xy_in_head, compress):
+    def get_metadata(self, asklist, metadatapath):
         '''
-        Add count column to xytable if not already present.
-        
-        Count gives the number of individuals of spp found at the point x, y. 
-        Note that the option to compress the data is not yet implemented.
+        Gets the relavent metadata from metadata file.
 
         Parameters
         ----------
-        xy_in_data : 2D ndarray
-            Array of xytable data with cols specified in xy_in_head
-        xy_in_head : list
-            List of headers for cols in xy_data
-        compress : bool
-            Flag to compress data so that there is only one row in xytable for 
-            each combination of spp and point coordinate.
+        asklist : list
+            List of tuples of column name and attribute, e.g., [('x', 
+            'precision'), ('y', 'maximum')].
 
-        Returns
-        -------
-        xy_out_data : 2D ndarray
-            Array of updated xytable data with cols specified in xy_out_head
-        xy_out_head : list
-            List of updated headers (with 'count' appended) for cols in xy_data
+        metadatapath : str
+            Path to the metadata file.
         '''
-        
-        # Check if header named count, if so do nothing, else add 1's
-        if 'count' not in xy_in_head:
-            rows = xy_in_data.shape[0]
-            xy_out_data = np.append(xy_in_data, np.ones((rows,1)), axis = 1)
-            xy_out_head = xy_in_head + ['count']
-        else:
-            xy_out_data = xy_in_data
-            xy_out_head = xy_in_head
 
-        # If not compress, return xy_out_data, else throw not implemented error
-        if not compress:
-            return xy_out_data, xy_out_head
-        else:
-            raise NotImplementedError('Compression of xytable not yet ' + 
-                                      'implemented')
+        meta_raw = metadata.Metadata(metadatapath)
+        meta_raw.get_dataTable_values(asklist)
+        meta_raw_dict = meta_raw.TableDescr
+
+        meta_dict = {}
+        for key, value in meta_raw_dict.iteritems():
+            meta_dict[key] = float(value)
+
+        return meta_dict
 
     def get_sub_table(self, x_st, x_en, y_st, y_en):
         '''
@@ -198,24 +129,3 @@ class XYTable():
                          < y_en], axis = 0)
         sub_table = self.table[in_sub]
         return sub_table
-
-    def fill_metadata(self, asklist, datapath):
-        '''
-        Gets the relavent metadata from metadata file
-
-        asklist -- A list of tuples e.g. [('x', 'precision'), ('y', 'maximum')]
-
-        datapath -- The path to the data file
-
-        '''
-        metadata_path = datapath[:-3] + 'xml'
-        meta = Metadata(metadata_path)
-        meta.get_dataTable_values(asklist)
-        meta_dict = meta.TableDescr
-        meta = {'precision' : float(meta_dict[('x', 'precision')]), \
-                    'xrange' : (float(meta_dict[('x', 'minimum')]), \
-                                float(meta_dict[('x', 'maximum')])), \
-                    'yrange' : (float(meta_dict[('y', 'minimum')]), \
-                                float(meta_dict[('y', 'maximum')]))}
-        return meta
-
