70ec6223c2506b224dea45e3757a5f096363a38b
Mark Wilber <mqwilber@gmail.com>
2012-08-01 11:05:26 -0700

    Fixed nu distribution in theory_energy

--- macroeco/theory/theory_energy.py
+++ macroeco/theory/theory_energy.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
 
 '''
-Calculate pmf and likilihood of METE energy distributions.
-
-All distributions have an argument summary, which if False returns the entire 
-pmf for the inputted values of n, and if true returns the summed negative 
-log-likelihood of the inputted values (useful for likelihood ratio tests or 
-AIC).
+METE energy distributions.
 
 Distributions
 -------------
 - 'mete_energy_theta_pdf -- The METE intra-specific energy distribution 
                             (Harte 2011)
+
+- 'mete_energy_theta_rank_abund'
+
 - 'mete_energy_psi_pdf' -- The METE community energy distribution
+
+-'mete_energy_psi_rank_abund'
 
 - 'mete_energy_nu_pdf/cdf' -- The METE mean energy per species distribution
 
@@ -24,11 +24,11 @@ Harte, J. 2011. Maximum Entropy and Ecology: A Theory of Abundance,
 Distribution, and Energetics. Oxford University Press. 
 
 '''
+
 from __future__ import division
 import numpy as np
 import scipy.optimize 
 import scipy.special
-import scipy.integrate as integrate
 import math as m
 import sys
 
@@ -95,14 +95,17 @@ def mete_energy_nu_pdf(S, N, E, param_out=False):
     Returns
     -------
     : ndarray (1D)
-        If testing is False, return a pdf.  If testing is True, returns pdf, 
-        lamda2, beta
+        If param_out is False, return a pdf.  If param_out is True, returns 
+        pdf, eta, and a dictionary with lamda2, beta
 
     Notes
     -----
     This function uses the nu energy equation found in in Table 7.3 of 
     Harte (2011). Nu is defined as the 'distribution of metabolic rates
     averaged over individuals within species' (Harte 2011). 
+
+    Because of its derivation, this function is discrete!  Only will return
+    certain values of e. 
 
     '''
 
@@ -119,22 +122,22 @@ def mete_energy_nu_pdf(S, N, E, param_out=False):
     #Getting beta
     k = np.linspace(1, N, num=N)
     eq = lambda x: sum(x ** k / float(N) * S) -  sum((x ** k) / k)
-    x = scipy.optimize.brentq(eq, start, min((sys.float_info[0] / S)**\
-                             (1/float(N)), stop), disp=True)
+    x = scipy.optimize.brentq(eq, start, min((sys.float_info[0] / S)**(1/float(N)),\
+                              stop), disp=True)
     beta = -m.log(x)
     lambda2 = float(S) / (E - N) #Harte (2011) 7.26
     eta_func = lambda n: 1 + (1 / (n * lambda2)) #Harte (2011) pg. 157
     eta = eta_func(np.arange(1, N + 1)[::-1])
-    pdf = (1 / np.log(1 / beta)) * ((np.exp(-(beta / (lambda2 * (eta - 1)))) /\
+    pdf = (1 / np.log(1 / beta)) * ((np.exp(-(beta / (lambda2 * (eta - 1)))) / \
                                     (eta - 1)))
     if param_out == True:
         return pdf, eta, {'lambda2' : lambda2, 'beta' : beta}
     else:
-        return pdf, eta
+        return pdf
     
-def mete_energy_nu_cdf(S, N, E, num_samples=100):
+def mete_energy_nu_cdf(S, N, E):
     '''
-    mete_energy_nu_cdf(S, N, E, num_samples=100)
+    mete_energy_nu_cdf(S, N, E)
 
     CDF for mete_energy_nu_pdf
 
@@ -146,52 +149,34 @@ def mete_energy_nu_cdf(S, N, E, num_samples=100):
         Total number of individuals in landscape
     E : float
         Total energy ouput of community
-    num_samples : int (default)
-        The number of integrals computed within the interval [emin,emax]
 
     Returns
     -------
     : structured ndarray (1D)
-        Structured array contains fields 'cdf' and 'energy'. The field 'cdf' 
-        representsthe cdf value at a given energy level 
-        (Prob (emin <= energy <= upppere)
+        Structured array contains fields 'cdf' and 'energy'. The field 'cdf' represents
+        the cdf value at a given energy level (Prob (emin <= energy <= upppere)
 
     Notes:
     ------
-    This function uses the equation in Harte (2011) Table 7.3 to calculate
-    emin and emax.  Note that because this equation is an approximation
-    the integral of the pdf from emin to max will often be less than one.
-
     '''
 
-    #Start and stop for root finders
     assert S < N, "S must be less than N"
     assert S > 1, "S must be greater than 1"
     assert N > 0, "N must be greater than 0"
     assert N < E, "N must be less than E"
 
-    start = 0.3
-    stop = 2
-
-    #Getting beta
-    k = np.linspace(1, N, num=N)
-    eq = lambda x: sum(x ** k / float(N) * S) -  sum((x ** k) / k)
-    x = scipy.optimize.brentq(eq, start, min((sys.float_info[0] / S)**\
-                             (1/float(N)), stop), disp=True)
-    beta = -m.log(x)
-    lambda2 = float(S) / (E - N) #Harte (2011) 7.26
-
-    pdf = lambda eta: (1 / np.log(1 / beta)) * ((np.exp(-(beta / (lambda2 * \
-                                    (eta - 1)))) / (eta - 1)))
+    pdf, epi, params = mete_energy_nu_pdf(S, N, E, param_out=True)
     cdf = []
-    emin = 1 + 1e-10
-    emax = E
-    #emax = 1 + (1 / (1 * lambda2))#Harte (2011), Table 7.3
-    #emin = 1 + (1 / (N * lambda2))
-    for eta in np.linspace(emin, emax, num=num_samples):
-        cdf.append((integrate.quad(pdf, emin, eta)[0], eta))
-
-    return np.array(cdf, dtype=[('cdf', np.float), ('energy', np.float)])
+    for i in xrange(len(pdf)):
+        if i < len(pdf) - 1:
+            cdf.append(pdf[i] * (epi[i + 1] - epi[i]))
+        else:
+            cdf.insert(0, 0)
+    cdf_struc = np.empty(len(pdf), dtype=[('epi', np.float), ('cdf',\
+                         np.float)])
+    cdf_struc['cdf'] = np.cumsum(np.array(cdf))
+    cdf_struc['epi'] = epi
+    return cdf_struc
 
 
 def mete_energy_psi_pdf(epi, S, N, E, param_out=True):
@@ -257,18 +242,16 @@ def mete_energy_psi_pdf(epi, S, N, E, param_out=True):
           exp_neg_gamma))))) # Harte (2011) 7.24
 
     #EW believes equation 7.24 may have an error
-    norm_factor = lambda2 / ((np.exp(-beta) - np.exp(-beta * (N + 1))) / (1 - \
-                  np.exp(-beta)) - (np.exp(-sigma) - np.exp(-sigma * (N + 1))) / \
-                  (1 - np.exp(-sigma)))
+    #norm_factor = lambda2 / ((np.exp(-beta) - np.exp(-beta * (N + 1))) / (1 - \
+    #              np.exp(-beta)) - (np.exp(-sigma) - np.exp(-sigma * (N + 1))) / \
+    #              (1 - np.exp(-sigma)))
 
-    pdf2 = norm_factor *  exp_neg_gamma * (1 - (N + 1) * exp_neg_gamma \
-           ** N + N * exp_neg_gamma ** (N + 1)) / \
-           (1 - exp_neg_gamma) ** 2 #EW Code
+    #pdf2 = norm_factor *  exp_neg_gamma * (1 - (N + 1) * exp_neg_gamma \
+    #       ** N + N * exp_neg_gamma ** (N + 1)) / \
+    #      (1 - exp_neg_gamma) ** 2 #EW Code
 
     #Based on some graphical analysis, there is a slight difference between the 
     #pdf as the values get higher. 
-
-
     if param_out == True: 
         return pdf1, {'lambda2' : lambda2, 'beta' : beta, 'lambda1' : lambda1,\
                       'sigma' : sigma}
@@ -358,7 +341,7 @@ def mete_energy_theta_rank_abund(S, N, E, n):
     '''
 
     l2 = float(S) / (E - N)
-    rank = np.arange(1, N + 1)
+    rank = np.arange(1, n + 1)
     rank_abund = lambda r : 1 + (1 / (l2 * n)) * np.log( n / (r - 0.5))
     theta_struc = np.empty(len(rank), dtype=[('rank', np.int), ('theta',\
                                                               np.float)])
