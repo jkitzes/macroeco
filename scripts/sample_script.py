c4b956b7f329d25e1d283552bdd7d0556af665f4
Justin Kitzes <jkitzes@berkeley.edu>
2012-08-18 20:01:12 -0700

    Fix few typos in sample_script and parameters

--- scripts/sample_script.py
+++ scripts/sample_script.py
@@ -12,6 +12,13 @@ To Use
 
 Best Practices
 --------------
+Required Parameters - The workflow object contains validation procedures that 
+will check to ensure that the parameters file in the output directory contains 
+all the parameters in a required_parameters dictionary in which entries are 
+'parameter_name':'short_description'. This dictionary is optional if the script 
+is run from the command line but required if it is run from the web GUI, in 
+which case it is used to allow users to create a params file from the GUI.
+
 Logging - To log the progress of the script, use logging.info('My message'), 
 where info may be replaced by other words to signify logging level (see Python 
 docs). By default, debug will print only to console and other levels will write 
@@ -30,6 +37,8 @@ long_descrip = '''This is a longer description of script actions, perhaps as
 long as a paragraph, that gives a detailed description of the purpose and 
 operation of this script.'''
 
+required_params = {'x': 'A sample numeric value'}
+
 import logging
 from macroeco.utils.workflow import Workflow
 
@@ -41,12 +50,12 @@ wf = Workflow()
 # data_path.
 for data_path, output_ID, params in wf.single_datasets():
 
-    x = 1 + 1
+    y = params['x'] + 1
 
     with open(wf.output_path + output_ID + '_results.txt', 'w') as file:
         file.write('Results\n\n')
         file.write('Data Path: %s\n\n' % data_path)
         file.write('Parameters: %s\n\n' % str(params))
-        file.write('x = %d\n\n' % x)
+        file.write('y = %f\n\n' % y)
 
     logging.info('Completed analysis %s' % output_ID)
