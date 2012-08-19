e9621b4199833ab60467e22c5a127ca922485fca
Justin Kitzes <jkitzes@berkeley.edu>
2012-08-18 19:36:46 -0700

    Update sample_script to run with new workflow

--- scripts/sample_script.py
+++ scripts/sample_script.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python
 
-'''Annotated, skeleton script showing how to run a particular
-analysis with a reproducible workflow.
+'''
+Sample script template showing use of Workflow object to enable reproducible 
+workflow.
 
-This is also the setup expected by the GUI.'''
+To Use
+------
+1.  Create an output directory to hold results of analysis
+2.  In this directory, create a parameters.xml file
+3.  Run this script with output directory as only sys arg
 
-import sys, os
-from macroeco.workflow import Workflow
-from macroeco import test
+Best Practices
+--------------
+Logging - To log the progress of the script, use logging.info('My message'), 
+where info may be replaced by other words to signify logging level (see Python 
+docs). By default, debug will print only to console and other levels will write 
+to logfile.txt in the output directory.
 
- 
-__author__ = "Chloe Lewis"
-__copyright__ = "Copyright 2012, Regents of the University of California"
-__license__ = None
-__version__ = "0.5"
-__maintainer__ = "Chloe Lewis"
-__email__ = "chlewis@berkeley.edu"
-__status__ = "Development"
+GUI Compatibility - For compatibility with the html GUI, the strings 
+'gui_name', 'short_descrip' and 'long_descrip' should appear in this script.
 
+'''
 
-# The workflow object takes care of logging, parameters, multiple runs, interactivity
-# Filenames are read from  the arguments used when this code was called (e.g., at the terminal)
-# Parameters are found in parameters.xml in the current working directory of the script
+gui_name = '''Sample Script'''
 
+short_descrip = '''This is a one sentence description of script actions.'''
+
+long_descrip = '''This is a longer description of script actions, perhaps as 
+long as a paragraph, that gives a detailed description of the purpose and 
+operation of this script.'''
+
+import logging
+from macroeco.utils.workflow import Workflow
+
+# Begin by creating a Workflow object
 wf = Workflow()
-# If the analysis happens file-by-file:
-for datafile, outputID, params in wf.single_datasets():
-    test.single_patch(datafile, outputID, params, wf.runs.interactive)
 
-# If the analysis combines all the files: 
-for datafiles, outputID, params in wf.all_datasets():
-    test.multiple_patch(datafiles, outputID, params)
+# Loop through each dataset specified in params and run analysis. If data_path 
+# is not a parameter, the loop below will run once with an empty string for
+# data_path.
+for data_path, output_ID, params in wf.single_datasets():
 
+    x = 1 + 1
 
-#Examples of logging:
-wf.logger.debug('Debug is logged to console only') #To console only
-wf.logger.info('Info and error are logged to file in working directory')
+    with open(wf.output_path + output_ID + '_results.txt', 'w') as file:
+        file.write('Results\n\n')
+        file.write('Data Path: %s\n\n' % data_path)
+        file.write('Parameters: %s\n\n' % str(params))
+        file.write('x = %d\n\n' % x)
 
-        
-            
-    
-
-
-
+    logging.info('Completed analysis %s' % output_ID)
