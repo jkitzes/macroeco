d900c9ba88eda2e05cee394f9e9a5b95f8446b1f
Justin Kitzes <jkitzes@berkeley.edu>
2012-08-18 18:39:35 -0700

    Update Parameters class

--- macroeco/utility/workflow.py
+++ macroeco/utility/workflow.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 '''
-Manages the details of a reproducible workflow within macroeco.
+Manages the details of a reproducible workflow within macroeco. Main Workflow 
+class is called with one argument, required_params, and the surrounding script 
+must be called with a single sys.argv with the output directory.
 
 Classes
 -------
@@ -26,24 +28,24 @@ __status__ = "Development"
 
 paramfile = 'parameters.xml'  # Name of parameter file found in output dir
 logfile   = 'logfile.txt'  # Name of logfile to save in output dir
-loggername = 'macroeco'
 
 
 class Workflow:
     '''
     Manages the details of a reproducible workflow with macroeco scripts.
-    Tracks which data set(s) are analyzed with which set(s) of parameters.
 
-    Parameters
+    Arguments
+    ---------
+    required_params : dictionary
+        Parameters needed for analysis, in form of 
+        'parameter_name':'short_description'. All of these parameters must be 
+        present in params file in output directory, or analysis will not run. 
+        This argument is empty only when no data or parameters are required for 
+        a script to run.
+        
+    Attributes
     ----------
-    asklist : dictionary
-        Parameters needed in form of 'parameter_name':'value'. If not given, 
-        all parameters recorded in parameter file for the current scriptname 
-        will be available.
-
-    Members
-    -------
-    script : string
+    script_name : string
         Name of script originating the workflow
     output_path : string
         Path to output directory
@@ -53,18 +55,17 @@ class Workflow:
         If parameters are needed, sets of parameter values are named runs
     '''
 
-    def __init__(self, asklist={}):
+    def __init__(self, required_params={}):
 
-        # Store script name using command line call
+        # Store script name from command line call
         script_path, script_extension = os.path.splitext(sys.argv[0])
-        self.script = os.path.split(script_path)[-1]
+        self.script_name = os.path.split(script_path)[-1]
 
         # Store output directory path - contains params file, log, results
         # TODO: If dir does not exist, create it? What if low level typo?
-        self.output_path = sys.argv[1]
-
-        # Store list of paths to data files
-        self.datafiles = sys.argv[2:]
+        # TODO: Make more robust to non-absolute path entries
+        output_path = sys.argv[1]
+        self.output_path = output_path
 
         # Prepare logger
         logging.basicConfig(filename=self.output_path + logfile, 
@@ -72,178 +73,205 @@ class Workflow:
                             %(levelname)s | %(filename)s:%(lineno)d | 
                             %(message)s''', datefmt='%H:%M:%S')
 
-        # Make map of sites if data files present
-        if len(sys.argv) < 3:
-            logging.info('No data files given, no site map generated')
-        else:
-            make_map(sys.argv[2:])
-        
-        # Try to get parameters from file
+        # Get parameters from file, including data paths
+        assert type(required_params) == type({})
+
         try:
-            self.runs = Parameters(self.script, asklist) #The asking stuff happens post-beta.
-            self.interactive = self.runs.interactive
-        except:
-            self.interactive = False
-            self.runs = None
-            
+            self.parameters = Parameters(self.script_name, required_params)
+            self.interactive = self.parameters.interactive
+        except:  # If no params file exists
+            logging.info('''No parameter file found at %s, proceeding without 
+                         parameters''' % output_path)
+            self.parameters = None
+            self.interactive = False            
         
     def single_datasets(self):
         '''
         Generator that yields data files and descriptive parameters.
 
+        Special parameter 'data_paths' is a list of locations of data files to 
+        use for analysis - if present, map of sites will be generated for each 
+        run.
+
         Yields
         ------
-        datafile : string
+        data_path : string
             Full path to data to analyze
         output_ID : string
             Concatenates script and dataset identifiers
+        run_params : dict
+            Dictionary of parameters for each script_name and run
         '''
 
-        def clean_name(fp):  # Extrace file name from path
+        def clean_name(fp):  # Extract file name from path
             return os.path.splitext(os.path.split(fp)[-1])[0]
 
-        if type(self.runs) == type(None):
-            for datafile in self.datafiles:
-                output_ID = '_'.join([self.script, clean_name(datafile)])
-                logging.debug('Beginning %s' % output_ID)
-                yield datafile, output_ID
-        else:
-            for run in self.runs.params.keys():
-                for datafile in self.datafiles:
-                    output_ID = '_'.join([self.script, clean_name(datafile), 
-                                          run])
-                    logging.debug('Beginning %s' % output_ID)
-                    yield datafile, output_ID, self.runs.params[run]
+        # Run script on all runs (parameter sets), and data sets
+        for run_name in self.parameters.params.keys():
+        # TODO: Check for output_ID conflicts (must be unique)
+
+            # Make map of sites if data file paths given in parameters
+            if 'data_paths' in self.parameters.params[run_name].keys():
+                make_map(self.parameters.params['data_paths'])
+            else:
+                logging.debug('''No data paths given for run %s, no map of 
+                              sites created''' % run_name)
+                
+            # Loop through each dataset and yield values for dataset and run
+            for data_path in self.parameters.params[run_name]['data_paths']:
+                output_ID = '_'.join([self.script_name, clean_name(data_path), 
+                                      run_name])
+                logging.info('Beginning %s' % output_ID)
+                yield data_path, output_ID, self.parameters.params[run_name]
 
         
 class Parameters:
-    '''Parameter values for any analysis:
-        asked for as a dictionary of name:helpstring,
-        available as self.params, a dictionary of name:value and "run_name":runname,
-        written to the current working directory in %s.
-    Parameters also tracks the run name, interactivity, and multiplicity.
+    '''
+    Load parameters from parameter file at output_path and make available as 
+    self.params. Checks that all required_params are present and loaded.
 
-    If %s is not sufficient, a dialog-box asks the user for values.'''%(paramfile, paramfile)
+    Arguments
+    ---------
+    script_name : string
+        Name of script originating the workflow
+    required_params : dictionary
+        Parameters needed for analysis, in form of 
+        'parameter_name':'short_description'. All of these parameters must be 
+        present in params file in output directory for this script_name and 
+        run, or analysis will not run. This argument is empty only when no data 
+        or parameters are required for a script to run.
+
+    Attributes
+    ----------
+    script_name : string
+        Name of script originating the workflow
+    interactive : bool
+        Whether the script can pause for user interaction
+    params : dict
+        Dictionary of dictionaries, with each outer key a run name and each 
+        outer value a dictionary of parameter names and values for each run.
+        
+    '''
     
-    def __init__(self, scriptname, asklist={}):
-        '''Builds a dictionary of dictionaries to satisfy the asklist.
+    def __init__(self, script_name, required_params):
 
-        Outer dictionary names are run names; inner are parameter name:value pairs.
-
-        Finds parameters in this order:
-
-            If there are interactive runs in %s for this scriptname:
-                open populated dialog, return results with run_name.
-
-            If there are only noninteractive runs in %s:
-                if they satisfy the asklist for this scriptname:
-                    return parameters with run_names added.
-                    
-
-        The argument asklist is a dictionary of 'name':'helpstring'.
-        Helpstrings should be short and explain what kind of value is needed, e.g., 
-            string,
-            value in a range,
-            value from a list.'''%(paramfile, paramfile)
-
-        assert type(asklist) == type({}) #TODO: Integration test
+        # Store initial attributes
+        self.script_name = script_name
         self.interactive = None
-        self.script = scriptname
         self.params = {}
-        self.logger = logging.getLogger(loggername)
-        
 
-        self.read_from_xml(asklist)
-        self.logger.debug('read parameters: %s'%str(self.params))
+        # Read parameter file
+        logging.info('Reading parameters from %s' % (output_path + paramfile))
+        self.read_from_xml()
 
-        #Make dict values into appropriate types
-        #NOTE: Possilbe Cryptic error if there is a typo in your parameter file
-        #If the eval() cannot eval the parameter it will retain its string form
-        for rn in self.params.iterkeys():
-            for key in self.params[rn].iterkeys():
-                try:
-                    value = eval(self.params[rn][key])
-                    self.params[rn][key] = value
-                    self.logger.debug('evaluated param %s' % key)
-                except:
-                    self.params[rn][key] = self.params[rn][key]
+        # Check that all required parameters present in all runs
+        if not self.required_params_present(required_params):
+            raise IOError('Required parameters missing')
+        logging.info('Parameters: %s' % str(self.params))
 
-        if not self.is_asklist_fulfilled:
-            self.logger.critical('Parameters missing from %s'%paramfile)
-        return
-        later='''        self.get_from_dialog(asklist)
-        if self.is_asklist_fulfilled:
-            self.write_to_xml()
-            return
-        
-            else:
-            raise Error #user wasn't helpful... TODO error type'''
+        # Evaluate param values into appropriate types
+        self.eval_params()
+
          
-    def read_from_xml(self, asklist):
+    def read_from_xml(self):
+        ''' Read parameters from xml file into self.params dictionary. '''
 
+        # Define class for checking keys
         class AllEntities:
             def __getitem__(self, key):
                 return key
 
+        # Check that parameter file is present and can be read
+        # TODO: Should this and try statements below just raise error w/o log?
         try:
-            pf = open(paramfile,'r')
+            pf = open(output_path + paramfile, 'r')
             pf.close()
         except IOError:
-            self.logger.error('Could not open %s'%paramfile) #Note; can't write is also an IOError
+            logging.error('Could not open parameter file %s' % (output_path + 
+                                                                paramfile))
             raise
 
-        parser = etree.XMLParser() # Without this, parsing works in iPython, console, not script. ??
+        # Declare parser object
+        # TODO: Without next line, works in iPython, console, not script ??
+        parser = etree.XMLParser()
         parser.parser.UseForeignDTD(True)
         parser.entity = AllEntities()
+
+        # Try to open paramfile from output_path
+        # TODO: Integration test
         try:
-            pml = etree.parse(paramfile, parser=parser).getroot() #Depends on cwd - #TODO integration test
+            pml = etree.parse(output_path + paramfile, parser=parser).getroot() 
         except etree.ParseError:
-            self.logger.error('ParseError trying to read %s'%paramfile)
+            logging.error('ParseError trying to read %s' % (output_path + 
+                                                            paramfile))
         except:
-            self.logger.error(sys.exc_info()[0])
-        runcount = 0
-        if len(pml) > 0:
-            for child in pml:
-                # check if any of these match scriptname TODO: and version (later)
-                if child.get('scriptname') == self.script:
-                    analysis = child
-                    if 'interactive' in child.attrib:
-                        ia = child.get('interactive')
-                        if ia == 'F' or ia == 'False' or ia == 'f' or ia == 'false':
-                            self.interactive = False
-                        else:
-                            self.interactive = True
+            logging.error(sys.exc_info()[0])
+        
+        # Create params dictionary
+        if len(pml) == 0:  # Error if no analyses in param file
+            raise IOError('Parameter file %s contains no valid analyses' % 
+                          (output_path + paramfile))
+
+        for analysis in pml:  # Loop analyses looking for script_name
+            if analysis.get('script_name') == self.script_name:
+
+                if 'interactive' in analysis.attrib:  # Set interactive
+                    ia = analysis.get('interactive')
+                    if ia in ['T', 'True', 't', 'true']:
+                        self.interactive = True
                     else:
-                        self.interactive = False #TODO: consider the default.
-                    if len(analysis) > 0:
-                        for run in analysis.getchildren():
-                            if 'name' in run.attrib:
-                                current_run = run.get('name')
-                            else:
-                                current_run = 'autoname'+str(runcount)
-                                runcount += 1
-                            self.params[current_run] = {}
-                            for elt in run.getchildren():
-                                if elt.tag == 'param':
-                                    self.params[current_run][elt.get('name')] = elt.get('value')
-        else:
-            self.logger.error("Need run entries in %s"%paramfile)
+                        self.interactive = False
+                else:
+                    self.interactive = False
 
-        # if none, *or* if interactive, put up dialog asking for values for all the params
+                if len(analysis) == 0:  # Error if no runs in analysis
+                    raise IOError('''Analysis found for this script, but no 
+                                  valid runs found''')
 
-        # on dialog OK, write out  -- replace run with same name (tricky!)
+                for run in analysis.getchildren():  # Loop runs
+                    run_name = run.get('name')
+                    self.params[run_name] = {}
+                    for elt in run.getchildren():  # Loop params in run
+                        if elt.tag == 'param':
+                            param = elt.get('name')
+                            value = elt.get('value')
+                            self.params[run_name][param] = value
 
-    def is_asklist_fulfilled(self):
-        if not set(self.params.keys()).issubset(set.self.asklist.keys()):
-            #some are missing
-            self.missing = ['Later','gator']
-            return False
-        else:
-            return True
+                            
+    def required_params_present(self, req_params):
+        ''' Check if any required parameters missing from any runs. '''
+
+        for run_name in self.params.keys():
+            run_params = self.params[run_name]
+            if not set(run_params.keys()).issubset(set(req_params.keys())):
+                return False
+        return True
+
+
+    def eval_params(self):
+        '''
+        Attempts to evaluate parameters to appropriate types.
+        
+        If eval() fails, parameter will stay a string, possibly leading to 
+        cryptic errors later if there is a typo in a param value.
+        '''
+
+        for run_name in self.params.keys():
+            for param_name in self.params[run_name].keys():
+                try:
+                    value = eval(self.params[run_name][param_name])
+                    self.params[run_name][param_name] = value
+                    value_type = str(type(value)).split("'")[1]
+                    self.logger.debug('In run %s, parameter %s evaluated to %s' 
+                                      % (run_name, param_name, value_type))
+                except:
+                    self.logger.debug('In run %s, parameter %s left as string' 
+                    % (run_name, param_name))
+
             
-def make_map(datalist, mapname = None, whole_globe = False):
+def make_map(datalist, mapname=None, whole_globe=False):
     '''
-    Ensures that a map of all the datasites being analyzed exists.
+    Makes a map of all sites in analysis.
 
     Parameter
     ---------
@@ -280,9 +308,6 @@ def make_map(datalist, mapname = None, whole_globe = False):
 
         lats.append(bounds[0])
         lons.append(bounds[1])
-
-
-
 
     print('Making map projection...')
     if whole_globe:
