78c42db85a90812df4724e76da5999631c04da68
Justin Kitzes <jkitzes@berkeley.edu>
2012-08-19 09:04:24 -0700

    Move test_metadata to test_data

--- scripts/macroeco/test_data.py
+++ scripts/macroeco/test_data.py
@@ -126,5 +126,27 @@ class TestXYTable(unittest.TestCase):
         np.testing.assert_array_equal(sub3, self.xyarr2)
         
 
+class MorphoMetadata(unittest.TestCase):
+    def setUp(self):
+        self.meta = metadata.Metadata('../../data/archival/BCIS/BCIS_1995.csv')        
+
+    def test_tablefromBCIS_Archival(self):
+        asklist = [('gx','precision'),
+                        ('gy','precision'),
+                        ('gx','minimum'),
+                        ('gx','maximum'),
+                        ('gy','maximum')]
+        values = ['0.1', '0.1','0','999.9','499.9']
+        expect = dict(zip(asklist, values))
+
+        self.meta.get_dataTable_values(asklist)
+        for ask in asklist:
+            assert self.meta.TableDescr[ask] == expect[ask]
+
+    def test_region(self):
+        edges = self.meta.get_coverage_region()
+        assert edges == [9.15,-79.85,9.15,-79.85]
+
+
 if __name__ == '__main__':
     unittest.main()
