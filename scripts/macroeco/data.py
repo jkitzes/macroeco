93e88e8caa2049a9b0ef2916d4a44d43c4be0c9e
Justin Kitzes <jkitzes@berkeley.edu>
2012-08-23 21:30:48 -0700

    Fix data module and ensure test_data runs with no errors
    Lots of changes made here, most importantly significant refactoring, ensuring
    that test_data runs, eval-ing metadata values from the asklist, and writing
    sample metadata directly from test_data rather than relying on BCI metadata for
    testing.

--- scripts/macroeco/data.py
+++ scripts/macroeco/data.py
@@ -10,104 +10,79 @@ Classes
 '''
 
 from __future__ import division
-import os, sys
+import os
 import logging
 import numpy as np
 import xml.etree.ElementTree as etree
 from matplotlib.mlab import csv2rec
 
+logging.disable(logging.CRITICAL)  # Disable any logging output
 
-class DataTable():
+class DataTable:
     '''
     Class to hold data table and metadata.
 
     Parameters
     ----------
-    datapath : str
-        Path to data. Location of metadata determined from this path.
+    data_path : str
+        Path to data - location of metadata determined from this path.
 
     Attributes
     ----------
+    asklist : list
+        A list of tuples of column name and attribute, e.g., [('x', 
+        'precision'), ('y', 'maximum')], that defines the columns and 
+        parameters that are needed for analysis. Defined in data_load method.
     table : recarray
-        Census data loaded from csv
-    meta_dict : dict
-        Dictionary of metadata associated with table.
+        Census data table.
+    meta : dict
+        Dictionary of metadata needed for analysis. Needed variables for each 
+        column are defined in asklist 
     '''
 
-    def __init__(self, datapath):
-        '''Initialize XYTable object. See class docstring.'''
-        self.table, self.meta = self.dataload(datapath)
+    def __init__(self, data_path):
+        '''Initialize DataTable object. See class docstring.'''
 
-    def dataload(self, datapath):
+        self.table, self.meta = self.data_load(data_path)
+
+
+    def data_load(self, data_path):
         '''
-        Load table and metadata from files.
+        Load data and metadata from files.
         
         Parameters
         ----------
-        datapath : str
+        data_path : str
             Path to data table file.
             
         Returns
         -------
         table : recarray
-            Census data loaded from csv.
+            Census data table.
         meta : dict
             Dictionary of metadata associated with table.
         '''
         
-        # Check that file is .csv.
-        assert datapath[-3:] == 'csv', 'File must be .csv'
+        # Check that file is csv
+        assert data_path[-4:] == '.csv', 'File must be .csv'
 
-        # Load main table
-        table = csv2rec(datapath)
+        # Load main table - dtype detected automatically
+        table = csv2rec(data_path)
 
-        # Load metadata from file, printing warning and returning dict of zeros 
-        # if file does not exist.
-        metadatapath = datapath[:-3] + 'xml'
-
-        asklist = []
+        # Store asklist defining columns and fields needed for analysis.
+        # asklist is
+        self.asklist = []
         for name in table.dtype.names:
-            asklist.append((name, 'minimum'))
-            asklist.append((name, 'maximum'))
-            asklist.append((name, 'precision'))
+            self.asklist.append((name, 'minimum'))
+            self.asklist.append((name, 'maximum'))
+            self.asklist.append((name, 'precision'))
+            self.asklist.append((name, 'type'))  # TODO: Confirm field name
+        
+        # Load metadata from file
+        meta = Metadata(data_path, self.asklist).meta_dict
 
-        meta_dict = self.get_metadata(asklist, metadatapath)
+        return table, meta
 
-        return table, meta_dict
-
-    def get_metadata(self, asklist, metadatapath):
-        '''
-        Gets the relavent metadata from metadata file.
-
-        Parameters
-        ----------
-        asklist : list
-            List of tuples of column name and attribute, e.g., [('x', 
-            'precision'), ('y', 'maximum')].
-
-        metadatapath : str
-            Path to the metadata file.
-        '''
-
-        meta_raw = Metadata(metadatapath)
-
-        # Check if metadata is missing, if so return None
-        if meta_raw.valid_file == False:
-            return None
-
-        meta_raw.get_dataTable_values(asklist)
-        meta_raw_dict = meta_raw.TableDescr
-
-        meta_dict = {}
-        for key, value in meta_raw_dict.iteritems():
-            if value is None:
-                new_value = value
-            else:
-                new_value = float(value)
-
-            meta_dict[key] = new_value
-
-        return meta_dict
 
     def get_sub_table(self, x_st, x_en, y_st, y_en):
         '''
@@ -129,83 +104,127 @@ class DataTable():
 
 class Metadata:
     '''
-    Metadata values for any analysis. Metadata is intrinsic to a dataset, 
-    and is stored using Ecological Markup Language. %citation TODO.
+    Metadata values for any analysis stored using Ecological Markup Language.
+
+    Parameters
+    ----------
+    data_path : str
+        Path to csv data file. Metadata file must be in same dir, with same 
+        filename, but with .xml extension.
     
+    Attributes
+    ----------
+    valid_file : bool
+        Whether valid metadata file was found.
+    root : object
+        Root of Element Tree representation of metadata xml file.
+    meta_dict : dict
+        Dictionary of metadata with values given by asklist.
+
     '''
 
-    def __init__(self, datapath):
-        ''' Parses the metadata from the EML file sibling to datapath (same 
-        location, same filename, .XML extension).
-
-        datapath is expected to be relative (from cwd).
-        
-        '''#TODO: handle absolute.
-        
-        dPath, dExtension = os.path.splitext(datapath)
-        xmlpath = os.path.abspath(os.path.join(dPath + '.xml'))
-    
+    def __init__(self, data_path, asklist):
+        '''Initialize Metadata object. See class docstring.'''
+       
+        # Get path to metadata file
+        data_path, data_extension = os.path.splitext(data_path)
+        xml_path = os.path.abspath(os.path.join(data_path + '.xml'))
+   
+        # Determine if metadata file is valid and if so store self.root
         self.valid_file = True
 
         try:
-            open(xmlpath)
+            open(xml_path)
         except:
-            logging.info('Missing or invalid metadata file at %s' % xmlpath)
+            logging.info('Missing or invalid metadata file at %s' % xml_path)
             self.valid_file = False
         
         try:
-            self.doc = etree.ElementTree(file=xmlpath)
-            self.root = self.doc.getroot()
+            self.root = etree.ElementTree(file=xml_path).getroot()
         except:
-            logging.info('Error parsing metadata file at %s' % xmlpath)
+            logging.info('Error parsing metadata file at %s' % xml_path)
+            self.root = None
             self.valid_file = False
+        
+        # Check if metadata file is missing or invalid, if so return None
+        if self.valid_file == False:
+            self.meta_dict = None
+        else:
+            self.meta_dict = self.get_meta_dict(asklist)
 
-	    self.TableDescr = None
 
+    def get_meta_dict(self, asklist):
+        '''
+        Parse metadata dictionary from xml file.
+        
+        Parameters
+        ----------
+        asklist : list
+            A list of tuples of column name and attribute, e.g., [('x', 
+            'precision'), ('y', 'maximum')], that defines the columns and 
+            parameters that are needed for analysis.
 
-    def get_dataTable_values(self, asklist):
-        ''' The asklist is pairs of column names and sub-attributes of that 
-        column, e.g. ('gx','precision').
+        Returns
+        -------
+        meta_dict : dict
+            Dictionary of metadata values for each item in asklist, in form 
+            {('column_name', 'element'): value}. column_name in data table is 
+            equivalent to attribute in xml.
+        '''
 
-        Metadata.TableDescr = {(columnName, subAttribute):subAttributeValue}'''
-        self.TableDescr = {}
-        for request in asklist:
-            print 'looking for %s in %s'%(request[1],request[0])
-            a = self.get_columnAtt_by_name(request[0])
-            if a is None:
-                v = None
+        # TODO: Column attribute will be None if either column entry does not
+        # exist in metadata or if column entry exists but attribute is missing. 
+        # We may want to distinguish these, perhaps just with logging.
+
+        # Populate dictionary of metadata values for asklist items
+        meta_dict = {}
+
+        for item in asklist:
+            # Get list of all elements for this attribute
+            all_elements = self.get_all_elements(item[0])
+
+            # Get value of element for this attribute if it exists
+            if all_elements is None:
+                value = None
             else:
-                v = self.get_subattribute_value(a, request[1])
-            self.TableDescr[request] = v
+                value = self.get_element_value(all_elements, item[1])
+            
+            # Eval value if possible and log outcome
+            try:
+                value = eval(value)
+                value_type = str(type(value)).split("'")[1]
+                logging.debug('Metadata value %s, %s evaluated to %s' % 
+                              (item[0], item[1], value_type))
+            except:
+                logging.debug('Metadata value %s, %s left as string' % 
+                              (item[0], item[1]))
+
+            # Store value for this item
+            meta_dict[item] = value
+
+        return meta_dict
 
 
-    def get_columnAtt_by_name(self, attribName):
-        '''
-        Returns list of XML elements of type attribute with the requested 
-        attributeName.
-        '''
+    def get_all_elements(self, attribute):
+        '''Returns list of XML elements of type attribute for attribute.'''
 
         attributes = self.root.findall('.//dataTable/attributeList/attribute')
         for a in attributes:
-            if a.find('.//attributeName').text == attribName:
+            if a.find('.//attributeName').text == attribute:
                 return a
-            #print 'No match found for ', attribName 
 
-    def get_subattribute_value(self, att, subatt):
-        '''
-        Returns the value of the subatt of the given att
-        '''
+
+    def get_element_value(self, all_elements, element_name):
+        '''Returns value of attribute_name from all_attributes list.'''
         try:
-            value = att.find('.//%s'%subatt).text
+            value = all_elements.find('.//%s' % element_name).text
             return value
         except AttributeError:
             return None
 
-    def get_coverage_region(self):
-        '''
-        Returns a tuple of the limits of the physical area of the dataset:  
-        NESW.
-        '''
+
+    def get_physical_coverage(self):
+        '''Returns a tuple of physical limits of the dataset (NESW).'''
         coords = self.root.find('.//coverage/geographicCoverage/' + 
                                 'boundingCoordinates')
         bounds = []
@@ -213,6 +232,7 @@ class Metadata:
             bounds.append(float(coords.find('%sBoundingCoordinate'%d).text))
         return bounds
 
+
     def get_title(self):
-        '''Extracts the title of the dataset'''
+        '''Extracts the title of the dataset. Not currently used.'''
         return self.root.find('.//dataset/title').text
--- scripts/macroeco/test_data.py
+++ scripts/macroeco/test_data.py
@@ -6,12 +6,12 @@ import unittest
 import os
 import numpy as np
 from matplotlib.mlab import csv2rec
-from macroeco.data import DataTable, Metadata
+from data import DataTable, Metadata
 
 class TestDataTable(unittest.TestCase):
 
     def setUp(self):
-        ''' Write test xytable csv files with and without count. '''
+        '''Write test xytable csv file.'''
 
         self.xyfile1 = open('xyfile1.csv','w')
         self.xyfile1.write('''spp_code, x, y
@@ -22,101 +22,105 @@ class TestDataTable(unittest.TestCase):
                        1, 1, 0''')
         self.xyfile1.close()
         self.xyarr1 = csv2rec('xyfile1.csv')
-        self.xyhead1 = ['spp_code', 'x', 'y']
-
-        self.xyfile2 = open('xyfile2.csv','w')
-        self.xyfile2.write('''spp_code, x, y, count
-                       0, 0, 0, 1
-                       0, 0, 0, 1
-                       0, 0, 1, 1
-                       1, 0, 0, 1
-                       1, 1, 0, 1''')
-        self.xyfile2.close()
-        self.xyarr2 = csv2rec('xyfile2.csv')
-        self.xyhead2 = ['spp_code', 'x', 'y', 'count']
-
-        self.xyfile3 = open('xyfile3.csv','w')
-        self.xyfile3.write('''bad, x, y, count
-                       0, 0, 0, 1
-                       0, 0, 0, 1
-                       0, 0, 1, 1
-                       1, 0, 0, 1
-                       1, 1, 0, 1''')
-        self.xyfile3.close()
-
-        self.xyfile4 = open('xyfile4.csv','w')
-        self.xyfile4.write('''spp_code, x, y, count
-                       0, .1, .1, 2
-                       0, .1, .2, 1
-                       0, .1, .3, 1
-                       2, .1, .2, 1
-                       2, .2, .3, 1''')
-        self.xyfile4.close()
-
-        self.xymeta = {'precision': 1, 'xrange': (0,1), 'yrange': (0,1)}
-        self.xymeta4 = {'precision': .1, 'xrange': (.1,.2), 'yrange': (.1,.3)}
 
     def tearDown(self):
         os.remove('xyfile1.csv')
-        os.remove('xyfile2.csv')
-        os.remove('xyfile3.csv')
-        os.remove('xyfile4.csv')
-        return
-
-    #
-    # xytable_load
-    #
 
     def test_error_if_file_type_not_csv(self):
         self.assertRaises(AssertionError, DataTable, 'file.txt')
         
-    def test_None_if_no_meta_file(self):
+    def test_meta_None_if_no_meta_file(self):
         xy1 = DataTable('xyfile1.csv')
         self.assertEqual(xy1.meta, None)
 
-    # TODO: Test writing and reading metadata file
+    def test_table_is_correct(self):
+        xy1 = DataTable('xyfile1.csv')
+        np.testing.assert_array_equal(xy1.table, self.xyarr1)
 
-    # TODO: Test throws error if loaded metadata is missing required key
-
-    #
-    # sub_table
-    #
-
-    def test_accurate_sub_tables(self):
-        xy2 = DataTable('xyfile2.csv')  # More complex table
-
-        sub1 = xy2.get_sub_table(0, 1, 0, 1)  # One cell
-        np.testing.assert_array_equal(sub1, self.xyarr2[[0, 1, 3]])
-        sub2 = xy2.get_sub_table(0, 1, 0, 2)  # Half plot
-        np.testing.assert_array_equal(sub2, self.xyarr2[[0, 1, 2, 3]])
-        sub3 = xy2.get_sub_table(0, 2, 0, 2)  # Whole plot
-        np.testing.assert_array_equal(sub3, self.xyarr2)
+#    def test_accurate_sub_tables(self):
+#        xy2 = DataTable('xyfile2.csv')  # More complex table
+#
+#        sub1 = xy2.get_sub_table(0, 1, 0, 1)  # One cell
+#        np.testing.assert_array_equal(sub1, self.xyarr2[[0, 1, 3]])
+#        sub2 = xy2.get_sub_table(0, 1, 0, 2)  # Half plot
+#        np.testing.assert_array_equal(sub2, self.xyarr2[[0, 1, 2, 3]])
+#        sub3 = xy2.get_sub_table(0, 2, 0, 2)  # Whole plot
+#        np.testing.assert_array_equal(sub3, self.xyarr2)
         
 
 class TestMetadata(unittest.TestCase):
-    # Uses BCIS archival metadata as sample
-    # TODO: Path below may not work on all OS
     
     def setUp(self):
-        self.meta = Metadata('../../data/archival/BCIS/BCIS_1995.csv')        
+        '''Write test data and metadata file.'''
 
-    # TODO: Will need to add check for datatype here also
-    def test_BCIS_Archival_metadata(self):
-        asklist = [('gx','precision'),
-                        ('gy','precision'),
-                        ('gx','minimum'),
-                        ('gx','maximum'),
-                        ('gy','maximum')]
-        values = ['0.1', '0.1','0','999.9','499.9']
-        expect = dict(zip(asklist, values))
+        self.xyfile1 = open('xyfile1.csv','w')
+        self.xyfile1.write('''x, y
+                       0, 0
+                       0, 0
+                       0, 0
+                       1, 0
+                       1, 1''')
+        self.xyfile1.close()
 
-        self.meta.get_dataTable_values(asklist)
-        for ask in asklist:
-            assert self.meta.TableDescr[ask] == expect[ask]
+        self.xymeta = open('xyfile1.xml','w')
+        self.xymeta.write('''<?xml version="1.0" encoding="UTF-8"?>
+<eml:eml packageId="macroeco.129.1" system="knb"                          
+xmlns:eml="eml://ecoinformatics.org/eml-2.1.0" 
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+xsi:schemaLocation="eml://ecoinformatics.org/eml-2.1.0 
+eml.xsd"><dataset>
 
-    def test_region(self):
-        edges = self.meta.get_coverage_region()
-        assert edges == [9.15,-79.85,9.15,-79.85]
+<title>Unittest XML</title> 
+
+<coverage><geographicCoverage><geographicDescription>NA</geographicDescription>
+<boundingCoordinates><westBoundingCoordinate>-79.5915</westBoundingCoordinate>
+<eastBoundingCoordinate>-79.5915</eastBoundingCoordinate>
+<northBoundingCoordinate>8.975</northBoundingCoordinate>
+<southBoundingCoordinate>10</southBoundingCoordinate>
+</boundingCoordinates>
+</geographicCoverage></coverage>
+
+<dataTable><attributeList>
+
+<attribute><attributeName>x</attributeName>
+<precision>0.1</precision>
+<numericDomain><numberType>real</numberType>
+<bounds><minimum exclusive="false">0</minimum>
+<maximum exclusive="false">99.9</maximum></bounds>
+<type>metric</type>
+</numericDomain>
+</attribute>
+
+</attributeList></dataTable>
+</dataset></eml:eml>''')
+        self.xymeta.close()
+
+    def tearDown(self):
+        os.remove('xyfile1.csv')
+        os.remove('xyfile1.xml')
+
+    def test_metadata_correct_read(self):
+        # Should read values correctly from sample file, including None for
+        # attributes that do not exist and elements that do not exist.
+        xy1 = DataTable('xyfile1.csv')
+        self.assertEqual(len(xy1.meta), 8)
+        self.assertEqual(xy1.meta, {('x', 'maximum'): 99.9,
+                                    ('x', 'minimum'): 0,
+                                    ('x', 'precision'): 0.1,
+                                    ('x', 'type'): 'metric',
+                                    ('y', 'maximum'): None,
+                                    ('y', 'minimum'): None,
+                                    ('y', 'precision'): None,
+                                    ('y', 'type'): None})
+
+    def test_physical_coverage(self):
+        meta = Metadata('xyfile1.csv', [])
+        edges = meta.get_physical_coverage()
+        self.assertEqual(edges, [8.975, -79.5915, 10, -79.5915])
+
+    def test_title(self):
+        meta = Metadata('xyfile1.csv', [])
+        self.assertEqual(meta.get_title(), 'Unittest XML')
 
 
 if __name__ == '__main__':
