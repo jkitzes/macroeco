f5600008defcd5fe4c9c397c0b7170f6f5e9a5e4
Justin Kitzes <jkitzes@berkeley.edu>
2013-04-25 20:40:58 -0700

    Add header info and submodule imports to init

--- __init__.py
+++ __init__.py
@@ -1,1 +1,14 @@
+__author__ = "Justin Kitzes, Mark Wilber, Chloe Lewis"
+__copyright__ = "Copyright 2012, Regents of University of California"
+__credits__ = []
+__license__ = "BSD 2-clause"
+__version__ = "0.1"
+__maintainer__ = "Justin Kitzes"
+__email__ = "jkitzes@berkeley.edu"
+__status__ = "Development"
 
+import compare
+import data
+import empirical
+import output
+import utils.workflow as workflow
