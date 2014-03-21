7f065bee779b957bdde25958ec6906c55ff72379
Justin Kitzes <jkitzes@berkeley.edu>
2014-03-20 19:39:31 -0700

    Add pandas, configparser to requirements, mock mpltools

--- doc/conf.py
+++ doc/conf.py
@@ -94,6 +94,31 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['mpltools']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 
 # -- Options for HTML output ---------------------------------------------------
 
