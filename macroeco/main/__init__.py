4122497f6124fc20f6b02e5fa34eed2236e57dbc
Justin Kitzes <jkitzes@berkeley.edu>
2014-03-24 18:46:52 -0700

    Move main doctoring to __init__

--- macroeco/main/__init__.py
+++ macroeco/main/__init__.py
@@ -1,1 +1,17 @@
+"""
+===========================
+Main (:mod:`macroeco.main`)
+===========================
+
+This module contains functions that execute macroecological analyses specified
+by user-generated `parameters.txt` configuration files. Instructions for
+creating parameter files can be found here.
+
+.. autosummary::
+   :toctree: generated/
+
+   main
+
+"""
+
 from .main import main
--- macroeco/main/main.py
+++ macroeco/main/main.py
@@ -1,19 +1,3 @@
-"""
-===========================
-Main (:mod:`macroeco.main`)
-===========================
-
-This module contains functions that execute macroecological analyses specified
-by user-generated `parameters.txt` configuration files. Instructions for
-creating parameter files can be found here.
-
-.. autosummary::
-   :toctree: generated/
-
-   main
-
-"""
-
 from __future__ import division
 import sys
 import os
