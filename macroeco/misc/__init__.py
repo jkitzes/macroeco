9c2496d5b66d66bd0ffcb61bdf54d2ee1d418d1d
Mark Wilber <mqwilber@gmail.com>
2014-04-01 21:46:29 -0700

    Fixed _init_ header

--- macroeco/misc/__init__.py
+++ macroeco/misc/__init__.py
@@ -1,8 +1,3 @@
-from .misc import *
-from .rcparams import *
-from format_data import (format_columnar, format_dense, format_grid,
-                        format_transect)
-=======
 """
 ===============================
 Misc (:mod:`macroeco.misc`)
@@ -24,5 +19,6 @@ other modules of macroeco.
 from .misc import (setup_log, _thread_excepthook, log_start_end,
                    inherit_docstring_from, doc_sub)
 from .rcparams import ggplot_rc
+from .format_data import (data_read_write, format_dense)
 
 _thread_excepthook()  # Make desktop app catch and log sys except from thread
