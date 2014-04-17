3f52aff4938cf9cb22733ea0b99940a6796106d7
Justin Kitzes <jkitzes@berkeley.edu>
2014-04-17 10:19:29 -0700

    Add subpackages to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description = 'Analysis of ecological patterns in Python',
     author = 'Justin Kitzes, Mark Wilber, Chloe Lewis',
     url = 'https://github.com/jkitzes/macroeco',
-    packages = ['macroeco'],
+    packages = ['macroeco', 'macroeco.empirical', 'macroeco.models',
+                'macroeco.compare', 'macroeco.main', 'macroeco.misc'],
     license = 'BSD',
 )
