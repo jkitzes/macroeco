f8bdd7c8139cfc6d7af4bb3d89e983073db976bf
Justin Kitzes <jkitzes@berkeley.edu>
2015-10-07 15:55:31 -0700

    Allow compiled OS X app to take parameter file as input on command line

--- mecodesktop.py
+++ mecodesktop.py
@@ -2,5 +2,10 @@
 MacroecoDesktop script for making standalone executable
 """
 
+import sys as _sys
 from macroeco import desktop
-desktop()
+
+if len(_sys.argv) > 1:
+    desktop(_sys.argv[1])
+else:
+    desktop()
